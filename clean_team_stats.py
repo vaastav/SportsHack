7c7625d2a8f5de754641281b59d9b2e8720bfa21
vaastav <vaastav.anand05@gmail.com>
2015-11-28 12:46:19 -0800

    Add attempted passes to the cleaned team stats

--- clean_team_stats.py
+++ clean_team_stats.py
@@ -4,11 +4,11 @@ def main():
   with open('cfl_team_stats.csv','r') as inf:
     reader = csv.DictReader(inf)
     with open('cleaned_team_stats.csv','w') as outf:
-      fieldnames = ['team_id','team_name','yards_per_play','pass_completion','yards_per_rush','yards_per_attempted_pass','yards_per_completed_pass']
+      fieldnames = ['team_id','team_name','yards_per_play','pass_completion','yards_per_rush','yards_per_attempted_pass','yards_per_completed_pass','attempted_passes']
       writer = csv.DictWriter(outf, delimiter=',',lineterminator='\n',fieldnames=fieldnames)
       writer.writeheader()
       for row in reader:
-	      writer.writerow({'team_id':row['team_id'],'team_name':str(row['city']) + ' ' + str(row['team_name']),'yards_per_play':row['game_totals_average'],'pass_completion':float(row['passing_completions'])/float(row['passing_attempts']),'yards_per_rush':row['rushing_average'],'yards_per_attempted_pass':row['passing_average'],'yards_per_completed_pass':float(row['passing_yards'])/float(row['passing_completions'])})
+	      writer.writerow({'team_id':row['team_id'],'team_name':str(row['city']) + ' ' + str(row['team_name']),'yards_per_play':row['game_totals_average'],'pass_completion':float(row['passing_completions'])/float(row['passing_attempts']),'yards_per_rush':row['rushing_average'],'yards_per_attempted_pass':row['passing_average'],'yards_per_completed_pass':float(row['passing_yards'])/float(row['passing_completions']),'attempted_passes':row['passing_attempts']})
 
 if __name__ == '__main__':
   main()
