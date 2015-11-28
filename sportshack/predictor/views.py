77a7ae3db1fb71d6c89ac8bc10ea9362b9bbd09f
Arabelle <arabelle310@yahoo.ca>
2015-11-28 15:29:00 -0800

    Configure the html pages

--- sportshack/predictor/views.py
+++ sportshack/predictor/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request, 'predictor/index.html')
+def home(request):
+	return render(request, 'predictor/home.html')
+def login(request):
+	return render(request, 'predictor/login.html')
+def signup(request):
+	return render(request, 'predictor/signup.html')
+
