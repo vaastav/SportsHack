6be2b2eb043828ca915edeea22d525ae3d923741
Arabelle <arabelle310@yahoo.ca>
2015-11-28 15:29:00 -0800

    Configure the html pages

---
+++ sportshack/predictor/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+	url(r'^$', views.index, name='index'),
+	url(r'^home/$', views.home, name='home'), # ADD NEW PATTERN!
+	url(r'^login/$', views.login, name='login'),
+	url(r'^signup/$', views.signup, name='signup'),
+]
